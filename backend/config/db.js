2aa2ec0fba7ccd9d0f4960c08bb35f0bbd6a793b
raghunathr1 <raghunthkapase2004@gmail.com>
2026-09-10 23:40:24 +0530

    Database Updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,14 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 
   waitForConnections: true,
   connectionLimit: 10,
