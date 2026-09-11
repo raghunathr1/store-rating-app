79d53102b5b150af17be0ca71c6321fa2cf91a07
raghunathr1 <raghunthkapase2004@gmail.com>
2026-09-11 11:45:02 +0530

    Deployment Process

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL = "https://store-rating-app-ew9z.onrender.com/api";
 
 // Common API request helper
 const apiRequest = async (url, options = {}) => {
