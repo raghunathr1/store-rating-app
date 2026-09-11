388be6749b6fba640c530ed2e7afe2504d7e3b2e
raghunathr1 <raghunthkapase2004@gmail.com>
2026-09-11 12:15:01 +0530

    Fix Credentials

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require("mysql2");
 
 const db = mysql.createPool({
   host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
@@ -18,12 +18,11 @@ const db = mysql.createPool({
 
 db.getConnection((err, connection) => {
   if (err) {
-    console.error("MySQL connection failed:", err.message);
+    console.error("MySQL connection failed:", err);
     return;
   }
 
   console.log("MySQL connected successfully");
-
   connection.release();
 });
 
